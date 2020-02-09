36d5ac8c629e3470238863524820695e7300f70a
gyujinLee <gjlee@openit.co.kr>
2020-02-10 08:48:05 +0900

    Life Cycle API (1) 추가사항

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React, { Fragment, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
+import MyComponent from './MyComponent';
 
   class App extends Component {
+
+    state = {
+      counter: 1,
+    }
+    
+    constructor(props) {
+      super(props);
+      console.log("constructor");
+    }
+
+    componentDidMount() {
+      console.log("componentDidMount");
+      // console.log(this.myDiv.getBoundingClientRect().height);
+    }
+
+    handleClick = () => {
+      this.setState({
+        counter : this.state.counter + 1
+      });
+    };
+
     render() {
-      return <Counter />;
+      return(
+        <div>
+          <MyComponent value={this.state.counter}/>
+          <button onClick={this.handleClick}>Click Me</button>
+        </div>
+      );
     }
   }
 
