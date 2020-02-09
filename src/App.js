6ecf7f0cd565c3da87f98d90129286470ce8002c
gyujinLee <gjlee@openit.co.kr>
2020-02-09 18:50:45 +0900

    input 상태 관리하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React, { Fragment, Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import Counter from './Counter';
+import PhoneForm from './components/PhoneForm';
 
-  class App extends Component {
-    render() {
-      return <Counter />;
-    }
+class App extends Component {
+  handleCreate = (data) => {
+    console.log("이름 : " + data.name);
+    console.log("전화번호 : " + data.phone);
   }
+
+  render() {
+    return (
+      <div>
+        {/* submit되면 onCreate라는 이벤트가 발생한다 */}
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
+      </div>   
+    );
+  }
+}
 
 export default App;
